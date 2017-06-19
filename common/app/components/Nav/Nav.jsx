5b4f37b56be15ab1835974bd742f0b40f1f8ad1d
Quincy Larson <michaelqlarson@gmail.com>
2017-06-19 16:11:47 -0500

    add points text but still need plural or 100x point multiplier

--- common/app/components/Nav/Nav.jsx
+++ common/app/components/Nav/Nav.jsx
@@ -73,7 +73,7 @@ export default class extends React.Component {
       <FCCNavItem
         className='brownie-points-nav'
         href={ '/' + username }>
-        [ { points } ]
+        { points } Points
       </FCCNavItem>
     );
   }
